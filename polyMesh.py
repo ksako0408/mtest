ab7f39276ebebed3ade59761a93a846f9e3ab477
ksako0408 <77800382+ksako0408@users.noreply.github.com>
2024-06-24 03:07:06 +0900

    Update polyMesh.py

--- polyMesh.py
+++ polyMesh.py
@@ -12,7 +12,6 @@ from operator import itemgetter
 import pprint
 import pandas as pd
 
-
 def read_polyMesh(processor, base_size, shift_shared, maxCellLevel_shared, lock_shared):
     points = read_PointsFile(processor)
     faces = read_FacesFile(processor)
@@ -44,11 +43,13 @@ def read_polyMesh(processor, base_size, shift_shared, maxCellLevel_shared, lock_
 
     p = np.array(points)
     p = (p - shift_shared) * normalized_Length
+    normalized_points = {i: np.array(
+                         (int(Decimal(v[0]).quantize(Decimal('0'), rounding=ROUND_HALF_UP)), \
+                          int(Decimal(v[1]).quantize(Decimal('0'), rounding=ROUND_HALF_UP)), \
+                          int(Decimal(v[2]).quantize(Decimal('0'), rounding=ROUND_HALF_UP))), \
+                          dtype = np.int32) for i, v in enumerate(p)}
 
-    normalized_points = {i: (int(Decimal(v[0]).quantize(Decimal('0'), rounding=ROUND_HALF_UP)), \
-                             int(Decimal(v[1]).quantize(Decimal('0'), rounding=ROUND_HALF_UP)), \
-                             int(Decimal(v[2]).quantize(Decimal('0'), rounding=ROUND_HALF_UP))) \
-                             for i, v in enumerate(p)}
+
 
     owner_face = np.arange(len(owner), dtype = np.int32)
     neighbour_face = np.arange(len(neighbour), dtype = np.int32)
@@ -58,80 +59,118 @@ def read_polyMesh(processor, base_size, shift_shared, maxCellLevel_shared, lock_
     neighbour_cell = np.array(neighbour, dtype = np.int32)
     cell_list = np.append(owner_cell, neighbour_cell)
 
-    unique_cell_list = np.unique(cell_list)
-    cell_face_list = [face_list[cell_list == n] for n in unique_cell_list]
-
-    cell_size_list = list()
-    cell_center_list = list()
-    for cell_face in cell_face_list:
-        px = set()
-        py = set()
-        pz = set()
-        for f in cell_face:
-            p_list = faces[f]
-            for p in p_list:
-                px.add(normalized_points[p][0])
-                py.add(normalized_points[p][1])
-                pz.add(normalized_points[p][2])
-        xmin = min(px)
-        ymin = min(py)
-        zmin = min(pz)
-        xmax = max(px)
-        ymax = max(py)
-        zmax = max(pz)
-        cell_size_list.append(xmax - xmin)
-        cell_center_list.append([int((xmax + xmin)/2), int((ymax + ymin)/2), int((zmax + zmin)/2)])
-
-    cell_center_list = np.array(cell_center_list, dtype = np.int32)
-    size_list = np.array([cell_size_list[n] for n in cell_list], dtype = np.int32)
-    center_list = np.array([cell_center_list[n] for n in cell_list], dtype = np.int32)
-
-
-    _ = [faces[n] for n in face_list]
-    face_point_list = [np.array([normalized_points[p] for p in face_point]) for face_point in _]
-    face_pminmax_list = [np.array([np.min(p, axis = 0), np.max(p, axis = 0)]) for p in face_point_list]
-
-
-    def fcsys(n, f):
-        f0 = face_pminmax_list[f][0]
-        f1 = face_pminmax_list[f][1]
-        bl = f0 == f1
-        cn = cell_center_list[n][bl][0]
-        fn = f0[bl][0]
-        sys = np.array([[1, 3], [2, 4], [5, 6]])
-        return sys[bl][0][np.array([cn < fn, fn < cn])][0]
-
-    cell_face_order = [[fcsys(n, f) for f in cell_face] for n, cell_face in zip(unique_cell_list, cell_face_list)]
-
-    if processor == "processor0":
-        pprint.pprint(cell_face_order)
-    #     pprint.pprint(cell_face_list[0])
-    #     pprint.pprint(face_pminmax_list[0])
-    #     pprint.pprint(cell_center_list[0])
-    #     pprint.pprint(face_pminmax_list[0][0][face_pminmax_list[0][0] == face_pminmax_list[0][1]])
-    #     pprint.pprint(cell_center_list[0][face_pminmax_list[0][0] == face_pminmax_list[0][1]])
-    #     pprint.pprint(face_point_list[0])
-        # pprint.pprint(cell_face_list)
-        # pprint.pprint(face_list)
-        # pprint.pprint(center_list)
-
-
-    Link_Wise_Lattice = pd.DataFrame(
+    polyMesh_df = pd.DataFrame(
         {
-            "cell_no":cell_list,
+            "cell": cell_list,
             "face": face_list,
-            "size": size_list,
-            "nx": center_list[:,0],
-            "ny": center_list[:,1],
-            "nz": center_list[:,2],
         }
     )
 
 
 
+    unique_face_list = np.unique(face_list)
+    unique_face_point_dict = {n: np.array([normalized_points[p] for p in faces[n]]) for n in unique_face_list}
+    unique_face_minmax_dict = {n: np.append(np.min(p, axis = 0), np.max(p, axis = 0)) for n, p in unique_face_point_dict.items()}
+    face_minmax_list = np.array([unique_face_minmax_dict[n] for n in face_list])
 
-    # if processor == "processor0":
-    #     print(Link_Wise_Lattice)
+    polyMesh_df["fxmin"] = face_minmax_list[:, 0]
+    polyMesh_df["fymin"] = face_minmax_list[:, 1]
+    polyMesh_df["fzmin"] = face_minmax_list[:, 2]
+    polyMesh_df["fxmax"] = face_minmax_list[:, 3]
+    polyMesh_df["fymax"] = face_minmax_list[:, 4]
+    polyMesh_df["fzmax"] = face_minmax_list[:, 5]
+
+
+
+    pmin_df = polyMesh_df[["cell", "fxmin", "fymin", "fzmin"]].groupby("cell").min()
+    pmax_df = polyMesh_df[["cell", "fxmax", "fymax", "fzmax"]].groupby("cell").max()
+
+    unique_cell_index = np.array(pmin_df.index, dtype = np.int32)
+    pmin = np.array(pmin_df)
+    pmax = np.array(pmax_df)
+    pmean = np.array([pmin, pmax]).mean(axis = 0, dtype = np.int32)
+    size = (pmax - pmin)[:, 0]
+
+    polyMesh_df["size"] = np.array([size[n] for n in cell_list])
+    polyMesh_df["nx"] = np.array([pmean[n] for n in cell_list])[:, 0]
+    polyMesh_df["ny"] = np.array([pmean[n] for n in cell_list])[:, 1]
+    polyMesh_df["nz"] = np.array([pmean[n] for n in cell_list])[:, 2]
+
+
+
+    def fcsys(pn, fmin, fmax):
+        bl = fmin == fmax
+        nc = pn[bl][0]
+        nf = fmin[bl][0]
+        sys = np.array([[1, 3], [2, 4], [5, 6]])
+        return sys[bl][0][[nc < nf, nf < nc]][0]
+
+    pn = np.array(polyMesh_df[["nx", "ny", "nz"]])
+    fmin = np.array(polyMesh_df[["fxmin", "fymin", "fzmin"]])
+    fmax = np.array(polyMesh_df[["fxmax", "fymax", "fzmax"]])
+
+    fcsys_list = np.array([fcsys(p, fm, fx) for p, fm, fx in zip(pn, fmin, fmax)], dtype = np.int32)
+    polyMesh_df["fcsys"] = fcsys_list
+
+
+
+    nInternalFaces = len(neighbour)
+    nFaces = np.array([v["nFaces"] for v in boundaries], dtype = np.int32)
+    startFace = np.array([v["startFace"] for v in boundaries], dtype = np.int32)
+    endFace = startFace + nFaces
+    boundaryType = [v["type"] for v in boundaries]
+    boundaryName = [v["name"] for v in boundaries]
+    def face_condition(faceNo):
+        if faceNo < nInternalFaces:
+            return ["InternalFace", "None"]
+        k = np.argmax(np.all([startFace <= faceNo, faceNo < endFace], axis = 0))
+        return [boundaryType[k], boundaryName[k]]
+
+    face_condition_list = np.array(list(map(face_condition, face_list)))
+    polyMesh_df["boundaryType"] = face_condition_list[:, 0]
+    polyMesh_df["boundaryName"] = face_condition_list[:, 1]
+
+
+
+
+
+    amp = size.reshape([len(size), 1])
+    pcsys_dict = {n: pmean + (amp * np.array(v)) for n, v in csys.items()}
+    tmp_list = np.concatenate(list(pcsys_dict.values()))
+    tmp_list = np.concatenate([pmean, tmp_list])
+    tmp_list = np.array(pd.DataFrame(data = tmp_list).drop_duplicates())
+    tmp_list = np.concatenate([pmean, tmp_list])
+    ghostCell_df = pd.DataFrame(data = tmp_list).drop_duplicates(keep = False)
+    ghostCell = np.array(ghostCell_df)
+
+    ns = np.max(unique_cell_index) + 1
+    ne = ns + len(ghostCell_df)
+    unique_ghost_index = np.arange(ns, ne, dtype = np.int32)
+
+    tmp_cell = np.concatenate([pmean, ghostCell])
+    tmp_index = np.append(unique_cell_index, unique_ghost_index)
+    pinv = {tuple(v): n for n, v in zip(tmp_index, tmp_cell)}
+    pcsys_tuple = tuple(map(tuple, np.concatenate([pcsys_dict[k] for k in sorted(list(csys.keys()))])))
+
+    ncell = len(unique_cell_index)
+    csys_cell_index = np.array([pinv[v] for v in pcsys_tuple], dtype = np.int32).reshape([ncell, -1], order = "F")
+    csys_cell_df = pd.DataFrame(csys_cell_index).add_prefix("csys_")
+    csys_cell_df["cell"] = unique_cell_index
+
+
+    Link_Wise_Lattice = pd.DataFrame(
+        {
+            "cell": unique_cell_index,
+            "size": size,
+            "nx": pmean[:,0],
+            "ny": pmean[:,1],
+            "nz": pmean[:,2],
+        }
+    )
+
+    Link_Wise_Lattice = Link_Wise_Lattice.merge(csys_cell_df)
+
+
 
 
 
